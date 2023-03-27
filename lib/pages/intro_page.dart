09723c3fcf0fee78399493a9667d91b972c157a3
Sefa Erkan <erkansefa138@gmail.com>
2023-03-27 17:44:06 +0300

    Grocery App

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'home_page.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 80,
+              right: 80,
+              bottom: 40,
+              top: 160,
+            ),
+            child: Image.asset(
+              "lib/images/avocado.png",
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Text(
+              "We deliver groceres at your doorstep",
+              textAlign: TextAlign.center,
+              style: GoogleFonts.notoSerif(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Text(
+            "Fresh items everyday",
+            style: TextStyle(
+              color: Colors.grey[600],
+            ),
+          ),
+          const Spacer(),
+          GestureDetector(
+            onTap: () => Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return const HomePage();
+                },
+              ),
+            ),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.deepPurple,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              padding: const EdgeInsets.all(24),
+              child: const Text(
+                "Get Started",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          const Spacer(),
+        ],
+      ),
+    );
+  }
+}
